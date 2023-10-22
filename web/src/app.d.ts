2724ec4692ec8f9a9a1b057330b60769c36306e0
DrPoppyseed <peaske16180@gmail.com>
2023-10-22 21:27:29 +0900

    update env

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -7,13 +7,6 @@ declare global {
         username?: string;
       };
     }
-
-    interface Platform {
-      env: {
-        VITE_PUBLIC_APP_SERVER_BASE_URL?: string;
-        VITE_PUBLIC_USER_AGENT_URL?: string;
-      };
-    }
   }
 }
 

a07999bd94f9a4f8e8625c7cf5e00442a30b49a5
DrPoppyseed <peaske16180@gmail.com>
2023-10-22 21:07:23 +0900

    use env file in web deploy

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -5,9 +5,16 @@ declare global {
     interface Locals {
       session: {
         username?: string;
-      }
+      };
+    }
+
+    interface Platform {
+      env: {
+        VITE_PUBLIC_APP_SERVER_BASE_URL?: string;
+        VITE_PUBLIC_USER_AGENT_URL?: string;
+      };
     }
   }
 }
 
-export {}
+export {};
--- web/src/lib/store.ts
+++ web/src/lib/store.ts
@@ -3,7 +3,7 @@ import type { RateLimits } from "./types";
 import { z } from "zod";
 
 export type Session = {
-  username: Option<string>;
+  username: string | null;
   isLoggedIn: boolean;
 };
 
@@ -18,13 +18,13 @@ export const rateLimits = writable<RateLimits>({
   userLimit: null,
   userRemaining: null,
   userReset: null,
-})
+});
 
 export const syncStateSchema = z.object({
   max: z.number(),
   min: z.number().optional().default(0),
   cur: z.number().optional().default(0),
-})
+});
 export type SyncState = z.infer<typeof syncStateSchema>;
 export const syncState = writable<SyncState>({
   max: 0,
